847429922283e86b41794ec1c365169db3a4cd34
shimizu <xxshimizuxx@gmail.com>
2017-03-03 11:17:06 +0900

    add sample tokyo

--- convert.sh
+++ convert.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 geojson-precision -p 4 $1 $1.prec
-cat $1.prec | simplify-geojson -t 0.0005 > $1.simp
+cat $1.prec | simplify-geojson -t 0.00001 > $1.simp
 node json2buf.js $1.simp
+rm -f $1.prec
+rm -f $1.simp
