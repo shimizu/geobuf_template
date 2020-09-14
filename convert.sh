91fa57f6df66cb045a7be6b0842fdfddd2292402
shimizu <xxshimizuxx@gmail.com>
2020-09-14 10:48:55 +0900

    ファイル整理

--- convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-geojson-precision -p 4 $1 $1.prec
-cat $1.prec | simplify-geojson -t 0.002 > $1.simp
-node json2buf.js $1.simp
-rm -f $1.prec
-rm -f $1.simp
---
+++ convert/convert.sh
@@ -0,0 +1,8 @@
+ #!/bin/sh
+
+ 
+ toposimplify -S 0.9 -F  $1.topo > $1_s.topo
+ topo2geo $1.geojson < $1_s.topo
+ node ../script/json2buf.js $1.geojson
+ #rm $1_s.topo
+ #rm $1.geojson
